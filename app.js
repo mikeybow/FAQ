a7e2a44cacdce1eaeebfc4528d5a2ac1ae7241fb
Michael Bowman <micbowman25@gmail.com>
2021-03-17 14:46:26 -0500

    finished

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-document.querySelectorAll(".faq").forEach((faq) => faq.addEventListener("click", () => {
-    if(faq.parentNode.classList.contains("active")) {
-        faq.parentNode.classList.toggle("active")
-    }
-    else{
-        document.querySelectorAll(".faq").forEach(faq => faq.parentNode.classList.remove("active"))
-        faq.parentNode.classList.add("active")
-    }
-}));
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+        toggle.parentNode.classList.toggle("active");
+    });
+});
